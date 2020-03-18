48250fa77b6d12da4ae16f6ed89f92233c446cb4
unknown <1524644382@qq.com>
2020-03-18 23:04:11 +0800

    Update

---
+++ 8.STML476RG_USART_DMA/MDK-ARM/8.STML476RG_USART_DMA/startup_stm32l476xx.d
@@ -0,0 +1,1 @@
+8.stml476rg_usart_dma\startup_stm32l476xx.o: startup_stm32l476xx.s
---
+++ 8.STML476RG_USART_DMA/MDK-ARM/RTE/_8.STML476RG_USART_DMA/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '8.STML476RG_USART_DMA' 
+ * Target:  '8.STML476RG_USART_DMA' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
